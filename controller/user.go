7c621348a1b1fa319cbb4c8c2af85000b8599f4b
shuxiaoning <shuxiaoningak@163.com>
2022-05-14 21:58:49 +0800

    this is demo of tmplate

--- controller/user.go
+++ controller/user.go
@@ -1,1 +1,31 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// usersLoginInfo use to store user info, and key is username+possword for demo
+// user data will be cleared every time the server starts
+// test data: username = zhanglei, possword  = douyin
+
+var usersLoginInfo = map[string]User{
+	"zhangleidouyin": {
+		Id:            1,
+		Name:          "zhanglei",
+		FollowCount:   10,
+		FollowerCount: 5,
+		IsFollow:      true,
+	},
+}
+
+func Login(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	token := username + password
+
+	if user, exist := usersLoginInfo[token]; exist {
+		c.JSON(http.StatusOK, user)
+	}
+}
