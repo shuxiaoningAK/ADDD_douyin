ddd92cb6e21bcc3a97046ec08cf6302bd87f7050
shuxiaoning <shuxiaoningak@163.com>
2022-05-17 00:12:45 +0800

    add mysql.ini

--- go.mod
+++ go.mod
@@ -20,5 +20,6 @@ require (
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
---
+++ util/setting.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"fmt"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode  string
+	HttpPort string
+
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	DbName     string
+)
+
+func init() {
+	file, err := ini.Load("../config/config.ini")
+	if err != nil {
+		fmt.Println("读取配置文件错误，请检查路径", err)
+	}
+
+	LoadServer(file)
+	LoadData(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("server").Key("AppMode").MustString("debug")
+	HttpPort = file.Section("server").Key("HttpPort").MustString(":8080")
+}
+func LoadData(file *ini.File) {
+	Db = file.Section("database").Key("Db").MustString("mysql")
+	DbHost = file.Section("database").Key("DbHost").MustString("")
+	DbPort = file.Section("database").Key("DbPort").MustString("3306")
+	DbUser = file.Section("database").Key("DbUser").MustString("root")
+	DbPassWord = file.Section("database").Key("DbPassWord").MustString("Sxn@163!")
+	DbName = file.Section("database").Key("DbName").MustString("douyin")
+
+}
