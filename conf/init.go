bfac15aa51415fe21df235819d2e172fb31f5db2
Amos <albert7king@gmail.com>
2022-05-18 22:18:32 +0800

    <add>:<开启了gorm中的预编译与禁用默认事务>

--- conf/init.go
+++ conf/init.go
@@ -1,15 +1,19 @@
 package conf
 
 import (
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"time"
 )
 
 var DB *gorm.DB
 
 func Database(connString string) {
-	if db, err := gorm.Open(mysql.Open(connString), &gorm.Config{}); err != nil {
+	if db, err := gorm.Open(mysql.Open(connString), &gorm.Config{
+		PrepareStmt:            true, //执行任何 SQL 时都创建并缓存预编译语句
+		SkipDefaultTransaction: true, //禁用默认功能
+	}); err != nil {
 		panic(err)
 	} else {
 		sqlDB, _ := db.DB()
